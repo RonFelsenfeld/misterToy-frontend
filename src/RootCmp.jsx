1507f81fb2c9ff8078e8cc9d519e225d6b4db24d
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-03-26 16:15:03 +0200

    Add basic app header

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,5 +1,17 @@
 import '../src/assets/style/main.css'
 
+import { Provider } from 'react-redux'
+
+import { store } from './store/store'
+
+import { AppHeader } from './cmps/AppHeader'
+
 export function App() {
-  return <section className="app">App</section>
+  return (
+    <Provider store={store}>
+      <section className="app">
+        <AppHeader />
+      </section>
+    </Provider>
+  )
 }
---
+++ src/cmps/AppHeader.jsx
@@ -0,0 +1,7 @@
+export function AppHeader() {
+  return (
+    <section className="app-header flex align-center">
+      <h1 className="main-title">Mister Toy</h1>
+    </section>
+  )
+}
